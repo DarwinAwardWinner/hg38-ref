b083abed75a924211c22a3864b1600fe96555b69
Ryan C. Thompson <rct@thompsonclan.org>
2016-09-01 12:59:36 -0700

    More robust code for getting versions

--- tool_versions.py
+++ tool_versions.py
@@ -1,59 +1,103 @@
+import re
+
 from localutils import *
 
 from subprocess import CalledProcessError
+
+def get_command_version_string(cmd, regexp, use_stderr=None,
+                               encoding=sys.getdefaultencoding()):
+    '''Get the version string from a command.
+
+    Arguments:
+
+    cmd: a string or list of strings to run a command that will
+    print a version number, typically something like 'mycommand
+    --version'.
+
+    regexp: A regular expression including a named capture group with
+    a name of 'version' that captures just the version string. For
+    example, if the command returns 'mycommand v1.2.3', the regexp
+    might be 'v(?P<version>(\\d+\\.)*\\d+)', which would match the
+    string '1.2.3' in the 'version' named capture group. If the regexp
+    doesn't match the output of the command, or is missing the named
+    capture group, an exception is raised.
+
+    use_stderr: If the command is known to print its version to
+    standard error instead of standard output, set this to True. If it
+    is known to print its version to standard output, set this to
+    False. If it is unknown, leave it as None, and the concatenation
+    of both (standard output first) will be searched for the version
+    string.
+
+    '''
+    use_shell = isinstance(cmd, str)
+    p = subprocess.Popen(cmd, shell=use_shell, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    if use_stderr is None:
+        output = stdout + stderr
+    elif use_stderr:
+        output = stderr
+    else:
+        output = stdout
+    output = output.decode(encoding)
+    m = re.search(regexp, output)
+    if m is None:
+        raise ValueError("Regular expression did not match command output")
+    return m.group("version")
+
 
 def shell_output_stripped(cmd):
     return check_output_decoded(cmd, shell=True).strip()
 
 # Determine the versions of various programs used
 try:
-    CUFFLINKS_VERSION = shell_output_stripped('''cufflinks 2>&1 | grep "cufflinks v"''')
-except CalledProcessError:
+    CUFFLINKS_VERSION = 'cufflinks ' + get_command_version_string('cufflinks --help', 'cufflinks v(?P<version>\S+)')
+except Exception:
     CUFFLINKS_VERSION = None
 
 try:
-    SAMTOOLS_VERSION = shell_output_stripped('''samtools 2>&1 | perl -lane 'print "samtools v$1" if m/Version: (.*)/' ''')
-except CalledProcessError:
+    SAMTOOLS_VERSION = 'samtools ' + get_command_version_string('samtools', 'Version:\\s+(?P<version>\\S+)')
+except Exception:
     SAMTOOLS_VERSION = None
 
 try:
-    BOWTIE1_VERSION = shell_output_stripped('''bowtie --version 2>/dev/null | head -n1''')
-except CalledProcessError:
+    BOWTIE1_VERSION = 'bowtie ' + get_command_version_string('bowtie --version', 'version\\s+(?P<version>\\S+)')
+except Exception:
     BOWTIE1_VERSION = None
 
 try:
-    BOWTIE2_VERSION = shell_output_stripped('''bowtie2 --version 2>/dev/null | head -n1 | perl -lane 'print "bowtie2 $1" if m/(version .*)/' ''')
-except CalledProcessError:
+    BOWTIE2_VERSION = 'bowtie2 ' + get_command_version_string('bowtie2 --version', 'version\\s+(?P<version>\\S+)')
+except Exception:
     BOWTIE2_VERSION = None
 
 try:
-    TOPHAT2_VERSION = shell_output_stripped('''tophat2 --version 2>/dev/null''')
-except CalledProcessError:
+    TOPHAT2_VERSION = 'tophat ' + get_command_version_string('tophat --version', 'TopHat v(?P<version>\\S+)')
+except Exception:
     TOPHAT2_VERSION = None
 
 try:
-    HISAT2_VERSION = shell_output_stripped('''hisat2 --version 2>/dev/null | head -n1 | perl -lane 'print "hisat2 $1" if m/(version .*)/' ''')
-except CalledProcessError:
+    HISAT2_VERSION = 'hisat2 ' + get_command_version_string('hisat2 --version', 'version\\s+(?P<version>\\S+)')
+except Exception:
     HISAT2_VERSION = None
 
 try:
-    BWA_VERSION = shell_output_stripped('''bwa 2>&1 | perl -lane 'print "bwa v$1" if m/Version: (.*)/' ''')
-except CalledProcessError:
+    BWA_VERSION = 'bwa ' + get_command_version_string('bwa', 'Version:\\s+(?P<version>\\S+)')
+except Exception:
     BWA_VERSION = None
 
 try:
-    BBMAP_VERSION = shell_output_stripped('''{BBMAP} --version 2>&1 | grep 'BBMap version' '''.format(BBMAP=BBMAP))
-except CalledProcessError:
+    BBMAP_VERSION = 'bbmap ' + get_command_version_string([BBMAP, '--version'], 'BBMap version (?P<version>\\S+)')
+except Exception:
     BBMAP_VERSION = None
 
 try:
-    STAR_VERSION = shell_output_stripped('''STAR --version 2>/dev/null''')
-except CalledProcessError:
+    STAR_VERSION = 'STAR ' + get_command_version_string('STAR --version', 'STAR_(?P<version>\\S+)')
+except Exception:
     STAR_VERSION = None
 
 try:
-    SALMON_VERSION = 'salmon ' + shell_output_stripped('''salmon --version 2>&1''')
-except CalledProcessError:
+    SALMON_VERSION = 'salmon ' + get_command_version_string('salmon --version', 'version\\s+:\\s+(?P<version>\\S+)')
+except Exception:
     SALMON_VERSION = None
 
 try:
